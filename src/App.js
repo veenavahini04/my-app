80fce162376ac8bdae72e6a9b16648cbf9011639
unknown <adepuveenavahii@gmail.com>
2024-12-21 11:55:00 +0530

    adding all files to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Apple from './Apple';
+
+function App() {
+  return (
+    <div className="App">
+      <Apple />
+
+    </div>
+  );
+}
+
+export default App;
